481ee4c83482c7b88534ae902bcc38aea810b544
X2nBryanChasko <bryan.chasko@x2nsat.com>
2022-12-19 16:00:12 -0800

    implimented error handling

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,16 +6,23 @@ import axios from "axios";
 //common pattern - fetcher function extrated outside of the usequery hook
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 
 export const RQSuperHeroesPage = () => {
   // useQuery contains isLoading and data hooks natively.
   // set a unique key super-heroes for the query, create a promise instance object
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes
+  );
 
   if (isLoading) {
     return <h2>Loading ...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -10,20 +10,31 @@ export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   // data variable holds the data fetched from the API
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   //react useEffect hook
   useEffect(() => {
     //make a get request to our API endpoint, which returns a promise we can respond to with .then
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      // setData to store the contents of the responde data res.data in the "data" variable declared above
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        // setData to store the contents of the responde data res.data in the "data" variable declared above
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   }, []);
 
   // use the state of the isLoading variable in order to return jsx indicating to the user the client is still loading data
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (

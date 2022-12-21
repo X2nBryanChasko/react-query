04ba13d838c008237627a4fda4d645e9f95a7949
X2nBryanChasko <bryan.chasko@x2nsat.com>
2022-12-21 10:55:41 -0800

    destructured our superherodata hook, and refernced our heroId as our querykey by its array position in the hook from our fetch function

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -3,7 +3,13 @@ import axios from "axios";
 
 //fetcher function we will call in our usequery below to hook to heroId
 
-const fetchSuperHero = (heroId) => {
+//destructure our query key
+const fetchSuperHero = ({ queryKey }) => {
+  // in our uequery hook below, we want our query key to map to the heroID
+  // returned from useQuery as our primary ID, and as super-hero and heroID
+  //are in array together, we can refer to the psoition of heroID as 1
+  // where super-hero object would be 0.
+  const heroId = queryKey[1];
   // this is the endpoint we will get all our information from on hero id
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
@@ -11,12 +17,8 @@ const fetchSuperHero = (heroId) => {
 //usequery hook
 //superhero data is equal to a function dependent on hero id, so we pass it in as a paramter
 export const useSuperHeroData = (heroId) => {
-  //return usequery with a key to identify the query, super-hero. also dependent on the heroId,
-  // we can specify an array to ensure that each superhero in an array of objects indexed by their Id rather than
-  // querying only the initial return object as 1 object. returning an array of super-hero
-  // indexed on heroId allows a dynamic return & reactQuery will now maintain separate queries for
-  // every hero that has an id
-  // we specify our fetcher function in a second argument as an arrow function
-  //  this functional also needs the heroId so it is fetching the appropriate details
-  return useQuery(["super-hero", heroId], () => fetchSuperHero(heroId));
+  //react-query automnaticly fetches other parameters, so we dont have to redeclare them in the arrow function.
+  return useQuery(["super-hero", heroId], fetchSuperHero);
+  //original code:
+  /* return useQuery(["super-hero", heroId], () => fetchSuperHero(heroId)); */
 };

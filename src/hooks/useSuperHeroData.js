42fb8769f610a3f86750da739b073558e00cd48e
X2nBryanChasko <bryan.chasko@x2nsat.com>
2022-12-21 16:22:43 -0800

    added initial query data to userSuperHeroData, so that when we load the list of superheroes, it loads the super hero details in the background, so we have an initial page load on the superheroes page, but the individual page datas render instantly out of cached data from the list

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 //fetcher function we will call in our usequery below to hook to heroId
@@ -14,11 +14,32 @@ const fetchSuperHero = ({ queryKey }) => {
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
 
-//usequery hook
-//superhero data is equal to a function dependent on hero id, so we pass it in as a paramter
+/* usequery hook
+superhero data is equal to a function dependent on hero id, so we pass it in as a paramter
+ call useQueryClient to get hold of client instance, queryClient isntance has access to
+ query cache, which we can now access to set initial data
+react-query automnaticly fetches other parameters, so we dont have to redeclare them in the arrow function.
+get hero from hero list by responding to hero paramter
+pass query key into queryClient's getQueryData function
+ ge tthe data from the list super-heroes query
+use optional chaining in case the cache doesn't have the super heroes list in the data object
+ find the hero whose hero id is the same as the passed in hero id from the url (parsed int to ensure numeric)
+ if we find a hero, we're goign to set a property called data to hero
+return undefined if hero is not found   */
 export const useSuperHeroData = (heroId) => {
-  //react-query automnaticly fetches other parameters, so we dont have to redeclare them in the arrow function.
-  return useQuery(["super-hero", heroId], fetchSuperHero);
+  const queryClient = useQueryClient();
+  return useQuery(["super-hero", heroId], fetchSuperHero, {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData("super-heroes")
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+      if (hero) {
+        return { data: hero };
+      } else {
+        return undefined;
+      }
+    },
+  });
   //original code:
   /* return useQuery(["super-hero", heroId], () => fetchSuperHero(heroId)); */
 };

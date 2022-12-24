2f1754c9a50848ecf9e6da6b98764992547b0ba7
X2nBryanChasko <bryan.chasko@x2nsat.com>
2022-12-23 16:53:04 -0800

    implimented axios interceptor to handle requests rather than in line api calls

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -2,4 +2,23 @@
 
 import axios from "axios";
 
-const clinet = axios.create({ baseURL });
+//specify an object and set a baseURL
+const client = axios.create({ baseURL: "http://localhost:4000" });
+
+//wrap all axios requets
+
+export const request = ({ ...options }) => {
+  //set an auth bearor token. in production you will likely retrieve this from local storage
+  client.defaults.headers.common.Authorization = "Bearer token";
+  //get our success response
+  const onSuccess = (response) => response;
+  //get the response of our error, which will dependent on implimentations
+  const onError = (error) => {
+    //optionally catch errors and add additional logging here
+    return error;
+  };
+
+  //return the axios client, passing in axios options and leveraging axios callbacks
+  // with onSuccess and onError, we have an axios interceptor ready.
+  return client(options).then(onSuccess).catch(onError);
+};
